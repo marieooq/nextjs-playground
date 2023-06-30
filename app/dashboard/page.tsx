07d87c8bd995cd8a3565311e3e4a69f75d1662de
marieooq <otakimarie@gmail.com>
2023-06-30 16:19:21 +0900

    added dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#000",
+        width: "100vw",
+        height: "100vh",
+        color: "yellow",
+      }}
+    >
+      Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
