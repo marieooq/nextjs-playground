dbe18e5e5d1207193309a4f7d013ec6453540199
marieooq <otakimarie@gmail.com>
2023-07-01 14:57:13 +0900

    created Setting and About page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="w-full h-screen bg-cyan-200 flex items-center justify-center">
+      <p className="text-9xl">About</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/setting/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Setting = () => {
+  return (
+    <div className="w-full h-screen bg-cyan-200 flex items-center justify-center">
+      <p className="text-9xl">Setting</p>
+    </div>
+  );
+};
+
+export default Setting;
