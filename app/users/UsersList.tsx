a87d4503a642f4695e04833140f58d215c651813
marieooq <otakimarie@gmail.com>
2023-08-06 11:29:34 +0900

    get getUsers function from page and put it into UsersList

---
+++ app/users/UsersList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const UsersList = async () => {
+  const getUsers = async (): Promise<User[]> => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    return response.json();
+  };
+  const users = await getUsers();
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,25 +1,10 @@
-type User = {
-  id: string;
-  name: string;
-  email: string;
-};
+import UsersList from "./UsersList";
 
-const Page = async () => {
-  const getUsers = async (): Promise<User[]> => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    return response.json();
-  };
-  const users = await getUsers();
-
-  console.log(users);
+const Page = () => {
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">User List</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <UsersList />
     </div>
   );
 };
